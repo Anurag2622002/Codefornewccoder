e9db3ae7e87ce4b9b694207e993a530d1a08b279
Ayush1347 <75846927+Ayush1347@users.noreply.github.com>
2022-10-04 20:08:44 +0530

    Create Fibonnaci.py

---
+++ Fibonnaci.py
@@ -0,0 +1,17 @@
+#Fibonacci series in Python
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# checking number is negative or positive if negative then enter positive number and if positive then print series
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
