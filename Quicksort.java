7ba3a5fcdbeffef9e89afbc005a72a8e2c220e77
Rudra Sharma <94120092+rudra016@users.noreply.github.com>
2022-10-04 13:34:46 +0530

    Updated QuickSort by adding Recusion.
    *Added Recursion.

--- Quicksort.java
+++ Quicksort.java
@@ -1,47 +1,45 @@
 import java.io.*;
-class GFG {
-	static void swap(int[] arr, int i, int j)
-	{
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}static int partition(int[] arr, int low, int high)
-	{
-	int pivot = arr[high];
-	int i = (low - 1);
+import java.util.Arrays;
 
-		for (int j = low; j <= high - 1; j++) {
-	if (arr[j] < pivot) {
-			i++;
-				swap(arr, i, j);
-			}
-		}
-		swap(arr, i + 1, high);
-		return (i + 1);
-	}
-	static void quickSort(int[] arr, int low, int high)
-	{
-		if (low < high) {
-		int pi = partition(arr, low, high);
-			quickSort(arr, low, pi - 1);
-			quickSort(arr, pi + 1, high);
-		}
-	}
-static void printArray(int[] arr, int size)
-	{
-		for (int i = 0; i < size; i++)
-			System.out.print(arr[i] + " ");
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+//        sort(arr, 0, arr.length - 1);
+//        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+    }
 
-		System.out.println();
-	}
+    static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
 
-	public static void main(String[] args)
-	{
-		int[] arr = { 10, 7, 8, 9, 1, 5 };
-		int n = arr.length;
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
 
-		quickSort(arr, 0, n - 1);
-		System.out.println("Sorted array: ");
-		printArray(arr, n);
-	}
+        while (s <= e) {
+
+            // also a reason why if its already sorted it will not swap
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        // now my pivot is at correct index, please sort two halves now
+        sort(nums, low, e);
+        sort(nums, s, hi);
+    }
 }
